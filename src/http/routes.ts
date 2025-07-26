3729504983b29a906b5384a8f3fd79ff221e4cc0
Marlon Buosi <marlonbuosi_@hotmail.com>
2025-07-25 22:00:39 -0300

    change route name

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 
 export async function appRoutes(app: FastifyInstance){
-  app.post('/register', register)
+  app.post('/orgs', register)
 }
