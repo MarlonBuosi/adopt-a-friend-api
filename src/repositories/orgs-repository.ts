03c4818964f04116e8a01373337155c4659f6a29
Marlon Buosi <marlonbuosi_@hotmail.com>
2025-08-26 17:11:57 -0300

    add missing file

--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -3,6 +3,6 @@ import { Org, Prisma } from '@prisma/client';
 
 export interface IOrgsRepository {
   create(data: Prisma.OrgCreateInput): Promise<Org>
-  findByEmail(name: string): Promise<Org | null>
+  findByName(name: string): Promise<Org | null>
 }
 
