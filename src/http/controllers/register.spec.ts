894c21cff777e2cda0e8c63abd6880da61daa642
Marlon Buosi <42485196+MarlonBuosi@users.noreply.github.com>
2025-09-30 17:07:02 -0700

    test: add base e2e testing to auth, profile and register controllers

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,28 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import { beforeAll, describe, expect, it } from "vitest";
+import request from 'supertest'
+import { after } from "node:test";
 
-test('testing', () => {
-  console.log('testing...')
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  after(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post(
+      '/orgs'
+    ).send(
+      {
+        name: 'Atila Org',
+        phone: '11999999999',
+        password: '123456789'
+      }
+    )
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
