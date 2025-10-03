0539186e485de1e804fe828a70382399b8642c77
Marlon Buosi <42485196+MarlonBuosi@users.noreply.github.com>
2025-10-02 18:35:21 -0700

    feat: add create pets controller

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,7 +1,10 @@
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+import { create } from "./create";
 
 
 export async function petsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.post('/pets', create)
 }
